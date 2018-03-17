b8bddcd46dc3e3bf3fe4e5299d951fcdaf3f3f64
marc <marc.vettiger@hotmail.com>
2018-03-17 20:21:34 +0100

    RAIFFEISEN FOND hardcoded

--- src/pages/investments/modalcontent.ts
+++ src/pages/investments/modalcontent.ts
@@ -22,16 +22,35 @@ import {Component} from "@angular/core";
         <ion-avatar item-start>
           <img src="{{character.image}}">
         </ion-avatar>
-        <h2>{{character.name}}</h2>
-        <p>{{character.quote}}</p>
+        <h2>{{character.bank}}</h2>
+        <p>{{character.name}}</p>
       </ion-item>
+    <ion-item>Risk<ion-note item-end>low</ion-note></ion-item>
+    <ion-item>Composition<ion-note item-end>worldwide<br/>shares<br/> bonds <br/>currencies</ion-note></ion-item>
+    <ion-item>Selection<ion-note item-end>ecological<br/>social <br/> ethical</ion-note></ion-item>
+    <ion-item>Proposition<ion-note item-end>100 CHF / month</ion-note></ion-item>
+    <ion-item>Potential Gain<ion-note item-end>5 years -> X <br/> 10 years -> Y </ion-note></ion-item>
+    <!--
       <ion-item *ngFor="let item of character['items']">
         {{item.title}}
-        <ion-note item-end>
-          {{item.note}}
+        <ion-note item-end *ngFor="let note of item.note">
+          {{note}}
         </ion-note>
       </ion-item>
+  -->
+    
+    
   </ion-list>
+  
+
+  <ion-footer align="center">
+    <ion-item>
+      <ion-icon name="alert" item-start></ion-icon>
+        Considered as 3rd pillar <br/>Tax deduction of X CHF
+    </ion-item>
+    
+    <button class="btn" block ion-button>Buy</button>
+  </ion-footer>
 </ion-content>
 `
 })
@@ -43,18 +62,27 @@ export class ModalContentPage {
     public params: NavParams,
     public viewCtrl: ViewController
   ) {
+
+    //const newline = "<br\>";
+
     let characters = [
       {
-        name: 'RAIFFEISEN',
-        quote: 'Pension Invest Futura',
+        bank: 'RAIFFEISEN',
+        name: 'Pension Invest Futura',
         image: 'assets/img/avatar-samwise.jpg',
         items: [
           { title: 'Risk', note: 'low' },
           { title: 'Composition', note: 'list required' },
-          { title: 'Selection', note: 'Sam' },
-          { title: 'Proposition', note: 'Sam' },
-          { title: 'Potential', note: 'Sam' },
-          { title: 'Gain', note: 'Sam' },
+          { title: 'Selection', note: 'Sam, Sam,' },
+          { title: 'Proposition', note: '100 CHF / month' },
+          { title: 'Potential Gain', note: ["5 years - > X", "< 10 years -> Y"] },
+          { title: 'Cost', note: 'Sam' },
+
+        ],
+        info: [
+
+          // place info text
+          // place button : Buy
         ]
 
       }
